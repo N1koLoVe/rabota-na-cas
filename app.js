4b4ccba7cfbe867e238267f6131fb4dbed79b43c
N1koLoVe <119735349+N1koLoVe@users.noreply.github.com>
2023-01-20 20:21:03 +0100

    AJAX recap

--- app.js
+++ app.js
@@ -1,319 +1,43 @@
-console.log("it works");
+const pokemonListEl = document.querySelector(".pokemon-list");
+const pokemonCardEl = document.querySelector(".pokemon-card");
 
-// Exercises
+const POKEMON_URL = "https://pokeapi.co/api/v2/pokemon";
 
-// Generating arrays
-
-function generateArray() {
-  let array = [];
-
-  for (let i = 1; i <= 1000; i++) {
-    if (i % 3 === 0) {
-      array.push(i);
-    }
+function renderPokemonList(listElement, pokemonData) {
+  for (let el of pokemonData) {
+    const newLi = document.createElement("LI");
+    newLi.textContent = el.name;
+    listElement.appendChild(newLi);
+    newLi.addEventListener("click", function () {
+      fetchPokemonByUrl(el.url);
+    });
   }
-  console.log(array);
 }
 
-generateArray();
-
-console.log("-------------------------------------");
-
-function generateArrayTwo() {
-  let arrayTwo = [];
-
-  for (let i = 1; i <= 1000; i++) {
-    if (i % 4 === 0 && i / 2) {
-      arrayTwo.push(i);
-    }
-  }
-  console.log(arrayTwo);
+function renderPokemonDetails(cardElement, pokemonDetailsData) {
+  cardElement.innerHTML = ` <h3>${pokemonDetailsData.name}</h3>
+    <img src="${pokemonDetailsData.sprites.front_default}" alt="image of a pokemon">`;
 }
 
-generateArrayTwo();
-
-console.log("-------------------------------------");
-
-function generateArrayThree() {
-  let arrayThree = [];
-
-  for (let i = 1; i <= 1000; i++) {
-    if (i % 10 === 1) {
-      arrayThree.push(i);
-    }
-  }
-  console.log(arrayThree);
+function fetchPokemons() {
+  console.log("before fetch");
+  fetch(POKEMON_URL)
+    .then(function (res) {
+      return res.json();
+    })
+    .then(function (data) {
+      console.log(data.results);
+      renderPokemonList(pokemonListEl, data.results);
+    });
+  console.log("after fetch");
 }
 
-generateArrayThree();
-
-console.log("-------------------------------------");
-
-// Clean arrays
-
-function cleanArray() {
-  let test = [
-    true,
-    false,
-    12,
-    13,
-    44,
-    2345,
-    "Bob",
-    "Jill",
-    false,
-    undefined,
-    1000,
-    null,
-    "Jack",
-    "",
-    "",
-    99,
-    "Greg",
-    undefined,
-    NaN,
-    1,
-    22,
-  ];
-
-  let cleanedArray = [];
-
-  for (let i = 0; i < test.length; i++) {
-    if (typeof test[i] === `string`) {
-      cleanedArray.push(test[i]);
-    }
-  }
-  console.log(cleanedArray);
+function fetchPokemonByUrl(url) {
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => {
+      renderPokemonDetails(pokemonCardEl, data);
+    });
 }
 
-cleanArray();
-
-console.log("-------------------------------------");
-
-function numArray() {
-  let test = [
-    true,
-    false,
-    12,
-    13,
-    44,
-    2345,
-    "Bob",
-    "Jill",
-    false,
-    undefined,
-    1000,
-    null,
-    "Jack",
-    "",
-    "",
-    99,
-    "Greg",
-    undefined,
-    NaN,
-    1,
-    22,
-  ];
-
-  let cleanedArray = [];
-
-  for (let i = 0; i < test.length; i++) {
-    if (typeof test[i] === `number`) {
-      cleanedArray.push(test[i]);
-    }
-  }
-  console.log(cleanedArray);
-}
-
-numArray();
-
-console.log("-------------------------------------");
-
-function undefinedArray() {
-  let test = [
-    true,
-    false,
-    12,
-    13,
-    44,
-    2345,
-    "Bob",
-    "Jill",
-    false,
-    undefined,
-    1000,
-    null,
-    "Jack",
-    "",
-    "",
-    99,
-    "Greg",
-    undefined,
-    NaN,
-    1,
-    22,
-  ];
-
-  let clearedArray = [];
-
-  for (let i = 0; i < test.length; i++) {
-    if (test[i]) {
-      clearedArray.push(test[i]);
-    }
-  }
-  console.log(clearedArray);
-}
-
-undefinedArray();
-
-console.log("-------------------------------------");
-
-// Random Color Page
-
-function randomColorHTML() {
-  const body = document.querySelector(`body`);
-  let r = Math.floor(Math.random() * 255);
-  let g = Math.floor(Math.random() * 255);
-  let b = Math.floor(Math.random() * 255);
-  body.style.backgroundColor = `rgb(${r},${g},${b})`;
-  body.innerHTML += `<h1>rgb(${r},${g},${b})</h1>`;
-}
-
-randomColorHTML();
-
-console.log("-------------------------------------");
-
-// Title Generator
-
-function generateTitle() {
-  const colorInput = document.querySelector("#color");
-  const fontSizeInput = document.querySelector("#fontSize");
-  const textInput = document.querySelector("#text");
-  const btn = document.querySelector("#btn");
-  const h1 = document.querySelector("#h1");
-
-  let color = colorInput.value;
-  let fontSize = fontSizeInput.value;
-  let text = textInput.value;
-  colorInput.value = ``;
-  fontSizeInput.value = ``;
-  textInput.value = ``;
-
-  h1.style.color = `${color}`;
-  h1.style.fontSize = `${fontSize}rem`;
-  h1.innerText = `${text}`;
-}
-
-console.log("-------------------------------------");
-
-// Student Constructor Function
-
-function Student(firstName, lastName, birthYear, academy, grades = []) {
-  this.firstName = firstName;
-  this.lastName = lastName;
-  this.birthYear = birthYear;
-  this.academy = academy;
-  this.grades = grades;
-  this.getAge = function () {
-    console.log(2023 - this.birthYear);
-  };
-  this.getInfo = function () {
-    console.log(
-      `This is student ${this.firstName} ${this.lastName} from the academy ${this.academy}!`
-    );
-  };
-  this.getGradesAverage = function () {
-    let sum = 0;
-    for (let i = 0; i < this.grades.length; i++) {
-      sum += this.grades[i];
-    }
-    sum = sum / this.grades.length;
-    console.log(sum);
-  };
-}
-
-const student = [
-  new Student("joce", "nikolov", 2003, "sedc", [2, 3, 4, 1, 2, 3]),
-  new Student("mirko", "velkov", 2005, "tama", [2, 5, 1, 2, 5]),
-  new Student("AH", "velksaasov", 2015, "tasama", [2, 5, 5, 5, 2, 5]),
-];
-
-student[0].getInfo();
-student[1].getInfo();
-student[2].getInfo();
-
-console.log("-------------------------------------");
-
-// List Generator From an Array
-
-function listGenerator() {
-  let names = [`Name 1`, `Name 2`, `Name 3`, `Name 4`];
-  let list = document.querySelector(".list-names");
-
-  names.forEach((name) => {
-    list.innerHTML += `
-        <ul><li>${name}</li></ul>
-        `;
-  });
-}
-
-console.log("-------------------------------------");
-
-// List Generator dynamically from inputs
-
-const inputColor = document.querySelector("#colors");
-const inputFont = document.querySelector("#fontSizes");
-const inputItems = document.querySelector("#items");
-const list = document.querySelector(".list");
-const button = document.querySelector(".btn");
-
-function dynamicList(color, fontSize, items) {
-  let liHTML = "";
-  for (let item of items) {
-    liHTML += `<li>${item}</li>`;
-  }
-  let listHTML = `<ul style = "color: ${color}; font-size: ${fontSize}rem">${liHTML}</ul>`;
-  list.innerHTML += listHTML;
-}
-button.addEventListener("click", function () {
-  const items = inputItems.value.split(",");
-  console.log(items);
-  dynamicList(inputColor.value, inputFont.value, items);
-});
-
-console.log("-------------------------------------");
-
-// Create a student registry page
-
-const fName = document.querySelector("#fname");
-const lName = document.querySelector("#lname");
-const age = document.querySelector("#age");
-const email = document.querySelector("#email");
-const registerBtn = document.querySelector("#register-btn");
-
-let database = [];
-
-function StudentGenerator(fName, lName, age, email) {
-  this.fName = fName;
-  this.lName = lName;
-  this.age = age;
-  this.email = email;
-}
-
-registerBtn.addEventListener("click", function () {
-  const newStudent = new StudentGenerator(
-    fName.value,
-    lName.value,
-    age.value,
-    email.value
-  );
-    fName.value = ``;
-    lName.value = ``;
-    age.value = ``;
-    email.value = ``;``
-
-  console.log(newStudent);
-  database.push(newStudent);
-});
-
-
+fetchPokemons();
