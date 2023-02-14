031bd07151a6ff08b830c55a5eadb37ccb41597b
N1koLoVe <119735349+N1koLoVe@users.noreply.github.com>
2023-02-14 13:19:37 +0100

    Add files via upload

---
+++ weather app/app.js
@@ -0,0 +1,119 @@
+const API_URL =
+  "https://api.openweathermap.org/data/3.0/onecall?lat=41.99646&lon=21.43141&units=metric&exclude=minutely&appid=83cf676a48739fd57b023a3d32f2ef8b";
+
+const ApiBtn = document.querySelector(".api-data");
+const date = new Date(1674727200 * 1000);
+console.log(date.toLocaleDateString("fr"));
+
+// ApiBtn.addEventListener("click", () => {
+//   fetch(API_URL)
+//     .then((res) => res.json())
+//     .then((data) => {
+//     //   homeBtn.addEventListener("click", () => {
+//     //     renderPage(data);
+//     //   });
+
+//     //   hourlyBtn.addEventListener("click", () => {
+//     //     renderHourly(data);
+//     //   });
+
+//     //   aboutBtn.addEventListener("click", () => {
+//     //     aboutMe();
+//     //   });
+//     // });
+// });
+
+const main = document.querySelector(".main");
+const homeBtn = document.querySelector(".home-btn");
+const nextDay = document.querySelector(".next-day");
+const hourlyBtn = document.querySelector(".hourly-btn");
+const aboutBtn = document.querySelector(".about-btn");
+
+const renderPage = (main, data) => {
+  const currentDate = new Date(data.current.dt * 1000);
+  main.innerHTML = "";
+  main.innerHTML = `
+  <div class="current-div">
+  <h1>Home</h1>
+  <h2>Current date : ${currentDate.toLocaleDateString(`fr`)}</h2>
+    <h4>Temperature : ${data.current.temp}</h4>
+    <h4>Feels like temperature : ${data.current.feels_like}</h4>
+    <h4>Wind speed : ${data.current.wind_speed}</h4>
+    <img src="http://openweathermap.org/img/wn/${
+      data.current.weather[0].icon
+    }@2x.png" alt="">
+    <h4>Weather description : ${data.current.weather[0].description}</h4>
+    <h4>Humidity : ${data.current.humidity}</h4>
+    <div class ="card-container"></div>
+    </div>`;
+
+  let dailyHTML = "";
+  for (let el of data.daily) {
+    const dailyDate = new Date(el.dt * 1000);
+    dailyHTML += `
+          <div class="day-box">
+          <h4>Date: ${dailyDate.toLocaleDateString("fr")}</h4>
+          <h4>Min Temp: ${el.temp.min}</h4>
+          <h4>Max Temp: ${el.temp.max}</h4>
+          <h4>Humidity: ${el.humidity}</h4>
+          <img src= "http://openweathermap.org/img/wn/${
+            el.weather[0].icon
+          }@2x.png" width = "150">
+          <h4>Description: ${el.weather[0].description}</h4>
+        </div>`;
+  }
+  document.querySelector(".card-container").innerHTML = dailyHTML;
+};
+
+const renderHourly = (main, data) => {
+  main.innerHTML = `<div class="hourly-div">
+  <h1>Hourly</h1>
+  <div class="hourly-card"></div>
+  </div>`;
+
+  let hourlyHtml = "";
+  for (let el of data.hourly) {
+    const hourlyInfo = new Date(el.dt * 1000);
+    hourlyHtml += `
+    <div class="hourly-box">
+  <h2>${hourlyInfo.toLocaleTimeString(`fr`)}</h2>
+    <h4>Temperature : ${el.temp}</h4>
+    <h4>Feels like temperature : ${el.feels_like}</h4>
+    <h4>Wind speed : ${el.wind_speed}</h4>
+    <img src="http://openweathermap.org/img/wn/${
+      el.weather[0].icon
+    }@2x.png" alt="">
+    <h4>Weather description : ${el.weather[0].description}</h4>
+    <h4>Humidity : ${el.humidity}</h4>
+    </div>`;
+  }
+  document.querySelector(".hourly-card").innerHTML = hourlyHtml;
+};
+
+const aboutMe = () => {
+  main.innerHTML = ``;
+  main.innerHTML = `
+  <div class="about">
+  <h1>Jordan Nikolov</h1>
+  <h3>Random guy trying to learn javascript, trying to become a streamer and a footballer</h3>
+  </div>`;
+};
+
+async function fetchApiWeather() {
+  const res = await fetch(API_URL);
+  const weatherData = await res.json();
+
+  homeBtn.addEventListener("click", () => {
+    renderPage(main, weatherData);
+  });
+
+  hourlyBtn.addEventListener("click", () => {
+    renderHourly(main, weatherData);
+  });
+
+  aboutBtn.addEventListener("click", () => {
+    aboutMe();
+  });
+}
+
+fetchApiWeather();
